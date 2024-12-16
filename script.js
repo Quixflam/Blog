d97b3a1e58b70453ce527a47e53606b366fa3aa5
Quixflam <comfortstitch.store@gmail.com>
2024-12-16 07:16:11 -0800

    Add Break Calculator & BG

---
+++ script.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+    Particles.init({
+        selector: ".background",
+        connectParticles: true,
+        maxParticles: 75,
+        speed: 0.3,
+        color: "#0044ff"
+    });
+};
